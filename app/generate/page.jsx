984d33ba76ca8f44485f4891a03f1b509b82bd3d
themuuln <zerone.offical@gmail.com>
2023-07-22 13:50:19 +0800

    generator added

---
+++ app/generate/page.jsx
@@ -0,0 +1,23 @@
+"use client";
+import QRCodeGenerator from "@/components/QRCodeGenerator";
+import { useState } from "react";
+
+export default function Home() {
+  const [url, setUrl] = useState("https://github.com/themuuln");
+
+  const qrInputHandler = (e) => {
+    console.log(e.target.value);
+    setUrl(e.target.value);
+  };
+  return (
+    <main className="flex flex-col items-center justify-center min-h-screen">
+      <div className="flex flex-col items-center space-y-4">
+        <input
+          value={url}
+          onChange={qrInputHandler}
+        />
+        <QRCodeGenerator value={url} />
+      </div>
+    </main>
+  );
+}
