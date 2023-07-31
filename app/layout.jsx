432feea66eae31c59458709550420018de6508e8
themuuln <zerone.offical@gmail.com>
2023-07-31 18:15:19 +0800

    tryna fix the bug

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,20 +1,14 @@
 "use client";
 import "./globals.css";
-import { Inter, Poppins } from "next/font/google";
+import { Poppins } from "next/font/google";
 import { ThemeProvider } from "@/components/theme-provider";
 import Navbar from "@/components/navbar/Navbar";
 import Footer from "@/components/footer/Footer";
 
-const inter = Inter({ subsets: ["latin"] });
 const poppins = Poppins({
   subsets: ["latin"],
   weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
 });
-
-export const metadata = {
-  title: "QR Generator",
-  description: "QR Generation Tool",
-};
 
 export default function RootLayout({ children }) {
   return (
