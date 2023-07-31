e6b5420c466bbc345630cc4014e1fd08283f364e
themuuln <zerone.offical@gmail.com>
2023-08-01 00:13:51 +0800

    home updated

--- app/page.jsx
+++ app/page.jsx
@@ -1,10 +1,12 @@
+import Footer from "@/components/footer/Footer";
+import Navbar from "@/components/navbar/Navbar";
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
-// import { BsGithub } from "react-icons/bs";
 
 export default function Home() {
   return (
     <>
+      <Navbar />
       <main className="container flex flex-col items-center justify-center min-h-screen">
         <div className="flex flex-col items-center justify-center space-y-4">
           <h1 className="text-4xl font-extrabold tracking-tight scroll-m-20 lg:text-5xl">
@@ -14,30 +16,32 @@ export default function Home() {
             A brief introduction to the QR Menu App and its purpose.
           </p>
 
-          <Link href="/generate">
-            <div className="space-x-3">
-              {/* <Button
+          <div className="space-x-3">
+            <Link href="/dashboard">
+              <Button
                 className="space-x-1"
                 variant="secondary"
               >
-                <BsGithub />
-                <span>Github</span>
-              </Button> */}
+                <span>Dashboard Preview</span>
+              </Button>
+            </Link>
+            <Link href="/menu">
+              <Button
+                className="space-x-1"
+                variant="secondary"
+              >
+                <span>Menu Preview</span>
+              </Button>
+            </Link>
+            <Link href="/generate">
               <Button>
                 <span>Get Started</span>
               </Button>
-            </div>
-          </Link>
+            </Link>
+          </div>
         </div>
-        {/* <div className="flex flex-col items-center justify-center space-y-4">
-          <h2 className="pb-2 text-3xl font-semibold tracking-tight transition-colors border-b scroll-m-20 first:mt-0">
-            Generate QR Code
-          </h2>
-          <Link href="/generate">
-            <Button>Generate</Button>
-          </Link>
-        </div> */}
       </main>
+      <Footer />
     </>
   );
 }
